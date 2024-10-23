9979cdb30ab206aba3140bfa37cc481a8d80da8b
turkey <abdulrhmanturkey123@gmail.com>
2024-10-23 13:47:18 +0200

    new depencies added
    documents loader for the app.py
    and add some requirements

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
- 
+from langchain_community.document_loaders import DirectoryLoader
+
+DATA_PATH = "data/books"
+
+loader = DirectoryLoader(DATA_PATH,glob="*.md")
+documents = loader.load()
+
+print(documents)
